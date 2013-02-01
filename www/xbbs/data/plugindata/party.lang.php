db3d2b57aa548d5b905c7cdb1e04e181b8d3ed37
leeyupeng <leeyupeng@gmail.com>
2013-02-01 08:37:19 +0800

    add party lang file

---
+++ www/xbbs/data/plugindata/party.lang.php
@@ -0,0 +1,23 @@
+<?php
+$scriptlang['party'] = array(     'add' => '添加',
+    'title' => '标题',
+    'content' => '内容',
+    'submit' => '提交',
+    'party_add' => '添加',
+    'party_config' => '版块召集设置',
+    'party_manage' => '活动管理',
+    'party_list' => '活动列表',
+    'success' => '添加成功',
+    'publish_time' => '发布时间',
+    'status' => '状态',
+    'publish' => '点击发布',
+    'in_publish' => '发布中',
+    'edit_success' => '消息已更换'
+);
+$templatelang['party'] = array(
+    'name' => 'yes'
+);
+$installlang['party'] = array(
+
+);
+?>
