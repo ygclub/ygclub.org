bcf1a01a9a619c5033833091ea0744dcee8bfbfe
leeyupeng <leeyupeng@gmail.com>
2013-08-02 21:58:40 +0800

    update icp num

--- www/wp/wp-content/themes/ygclub/footer.php
+++ www/wp/wp-content/themes/ygclub/footer.php
@@ -4,8 +4,8 @@
 </div>
 <div id="bottom"></div>
 <div id="copyright">
-	&copy;2010 阳光志愿者. 版权所有.<br />
-    京ICP备06025111号 | 关于我们 | 联系我们
+	&copy;2013 阳光志愿者. 版权所有.<br />
+    京ICP备13018722号-2 | 关于我们 | 联系我们
 </div>
 </body>
 </html>
