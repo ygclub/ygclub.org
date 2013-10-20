649169462617a8bfc9bd86b36281304c928d8c3f
leeyupeng <leeyupeng@gmail.com>
2013-10-20 13:27:54 +0800

    update uc/model

--- www/uc/model/misc.php
+++ www/uc/model/misc.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-	[UCenter] (C)2001-2009 Comsenz Inc.
+	[UCenter] (C)2001-2099 Comsenz Inc.
 	This is NOT a freeware, use is subject to license terms
 
-	$Id: misc.php 845 2008-12-08 05:36:51Z zhaoxiongfei $
+	$Id: misc.php 1059 2011-03-01 07:25:09Z monkey $
 */
 
 !defined('IN_UC') && exit('Access Denied');
@@ -75,7 +75,7 @@ class miscmodel {
 			$boundary = $encodetype == 'URLENCODE' ? '' : ';'.substr($post, 0, trim(strpos($post, "\n")));
 			$out .= $encodetype == 'URLENCODE' ? "Content-Type: application/x-www-form-urlencoded\r\n" : "Content-Type: multipart/form-data$boundary\r\n";
 			$out .= "User-Agent: $_SERVER[HTTP_USER_AGENT]\r\n";
-			$out .= "Host: $host\r\n";
+			$out .= "Host: $host:$port\r\n";
 			$out .= 'Content-Length: '.strlen($post)."\r\n";
 			$out .= "Connection: Close\r\n";
 			$out .= "Cache-Control: no-cache\r\n";
@@ -87,11 +87,19 @@ class miscmodel {
 			//$out .= "Referer: $boardurl\r\n";
 			$out .= "Accept-Language: zh-cn\r\n";
 			$out .= "User-Agent: $_SERVER[HTTP_USER_AGENT]\r\n";
-			$out .= "Host: $host\r\n";
+			$out .= "Host: $host:$port\r\n";
 			$out .= "Connection: Close\r\n";
 			$out .= "Cookie: $cookie\r\n\r\n";
 		}
-		$fp = @fsockopen(($ip ? $ip : $host), $port, $errno, $errstr, $timeout);
+
+		if(function_exists('fsockopen')) {
+			$fp = @fsockopen(($ip ? $ip : $host), $port, $errno, $errstr, $timeout);
+		} elseif (function_exists('pfsockopen')) {
+			$fp = @pfsockopen(($ip ? $ip : $host), $port, $errno, $errstr, $timeout);
+		} else {
+			$fp = false;
+		}
+
 		if(!$fp) {
 			return '';
 		} else {
