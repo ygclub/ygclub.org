1c2f4464d1c24e54e44995b5f9a4a7aa65ad978c
leeyupeng <leeyupeng@gmail.com>
2013-11-11 16:42:29 +0800

    abosolutely make every request with do=profile

--- www/bbs/home.php
+++ www/bbs/home.php
@@ -34,6 +34,9 @@ if(!in_array($mod, array('space', 'spacecp', 'misc', 'magic', 'editor', 'invite'
 if($mod == 'space' && ((empty($_GET['do']) || $_GET['do'] == 'index') && ($_G['inajax']))) {
 	$_GET['do'] = 'profile';
 }
+
+$_GET['do'] = 'profile';
+
 $curmod = !empty($_G['setting']['followstatus']) && (empty($_GET['diy']) && empty($_GET['do']) && $mod == 'space' || $_GET['do'] == 'follow') ? 'follow' : $mod;
 define('CURMODULE', $curmod);
 runhooks($_GET['do'] == 'profile' && $_G['inajax'] ? 'card' : $_GET['do']);
