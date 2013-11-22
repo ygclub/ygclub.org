4e7d49172ddc1869653f54fbe326df695229b530
leeyupeng <leeyupeng@gmail.com>
2013-11-23 02:42:36 +0800

    add project stat

--- www/bbs/act.php
+++ www/bbs/act.php
@@ -14,9 +14,175 @@ $starttime = $_GET['starttime'];
 $endtime = $_GET['endtime'];
 $uid = $_GET['uid'];
 $username = $_GET['username'];
+$list_type = $_GET['type'];
+$typeid = $_GET['typeid'];
+$today = strtotime(date('Y-m-d')) + 86399;
+
+if($list_type == 'project')
+{
+    $navigation .= ' <em>&rsaquo;</em> <a href="act.php?type=project">项目活动统计<a/>';
+    if($typeid > 0)
+    {
+        $this_year = date('Y');
+        $param_list = array();
+        $index = 0;
+        for($i=$this_year; $i>=2010; $i --)
+        {
+            if($i == $this_year)
+            {
+                if(date('m') > 9)
+                {
+                    $param_list[$index]['param'] = "&year=$i&half=f";
+                    $param_list[$index]['name'] = "{$i}年秋季学期";
+                }
+                $index ++;
+                $param_list[$index]['param'] = "&year=$i&half=s";
+                $param_list[$index]['name'] = "{$i}年春季学期";
+                $index ++;
+            }
+            else
+            {
+                $index ++;
+                $param_list[$index]['param'] = "&year=$i&half=f";
+                $param_list[$index]['name'] = "{$i}年秋季学期";
+                $index ++;
+                $param_list[$index]['param'] = "&year=$i&half=s";
+                $param_list[$index]['name'] = "{$i}年春季学期";
+            }
+        }
+        if($_GET['year'] == '')
+        {
+            $current_time = time();
+        }
+        else
+        {
+            if($_GET['half'] == 's')
+            {
+                $current_time = strtotime($_GET['year'] . '-03-01');
+            }
+            else
+            {
+                $current_time = strtotime($_GET['year'] . '-09-01');
+            }
+        }
+        $current_month = date('m', $current_time);
+        $spring_half_start = strtotime(date('Y',$current_time).'-03-01');
+        $spring_half_end = strtotime(date('Y',$current_time).'-08-31');
+        $fall_half_start = strtotime(date('Y',$current_time).'-09-01');
+        $fall_half_end = strtotime(date('Y',$current_time).'-02-28' . ' +1 year');
+        $sql = 'SELECT p.tid, p.showtime, t.typeid, t.subject, c.name  FROM ';
+        $sql .= DB::table('ygclub_party') . " as p ";
+        $sql .= "LEFT JOIN " . DB::table('forum_thread') . " as t on p.tid = t.tid ";
+        $sql .= "LEFT JOIN " . DB::table('forum_threadclass') . " as c on t.typeid = c.typeid where 1 ";
+        $sql .= "AND t.fid=2 ";
+        $sql .= "AND t.subject NOT LIKE '%活动取消%' ";
+        $sql .= "AND p.class = '阳光公益活动' ";
+        $sql .= "AND t.typeid = '$typeid' ";
+
+        if($current_month  >=3 && $current_month <= 8) 
+        {
+            $season = '春季';
+            $sql .= "AND p.showtime >= $spring_half_start ";
+            $sql .= "AND p.showtime <= $spring_half_end ";
+            $current_year = date('Y', $spring_half_start);
+        }
+        else
+        {
+            $season = '秋季';
+            $sql .= "AND p.showtime >= $fall_half_start ";
+            $sql .= "AND p.showtime <= $fall_half_end ";
+            $current_year = date('Y', $fall_half_start);
+        }
+
+        $sql .= "ORDER BY p.showtime ASC ";
+
+        $result = DB::fetch_all($sql);
+        $project_act = array();
+        $party_tid_array = array(0);
+        $project_name = '';
+        foreach($result as $act_info)
+        {
+            $project_name = $act_info['name'];
+            $party_tid_array[] = $act_info['tid'];
+            $project_act[$act_info['tid']] = $act_info;
+            $project_act[$act_info['tid']]['date_time'] = date('Y-m-d',$act_info['showtime']);
+        }
+        $party_tids = join(',', $party_tid_array);
+        $project_act_list_count = count($project_act);
+        
+        $sql = 'SELECT uid, username, usertask, tid FROM ';
+        $sql .= DB::table('ygclub_partyers') . ' WHERE 1 ';
+        $sql .= "AND verified = 4 ";
+        $sql .= "AND checkin = 1 ";
+        $sql .= "AND tid IN ($party_tids) ";
+        $sql .= "ORDER BY tid ASC, dateline ASC, uid ASC ";
+        $result = DB::fetch_all($sql);
+        $member_act = array();
+        $member_act_total_count = array();
+        foreach($result as $act_info)
+        {
+            if(trim($act_info['usertask']) == '') $act_info['usertask'] = '未指定分工';
+            $member_act[$act_info['uid']]['username']= $act_info['username'];
+            $member_act[$act_info['uid']]['total_act_count']  ++;
+            if($project_act[$act_info['tid']]['usertask'][$act_info['usertask']] == '')
+            { 
+                $project_act[$act_info['tid']]['usertask'][$act_info['usertask']] .= $act_info['username'];
+            }
+            else
+            {
+                $project_act[$act_info['tid']]['usertask'][$act_info['usertask']] .= ", " . $act_info['username'];
+            }
+            $project_act[$act_info['tid']]['user_act_count'] ++;
+            $weight = 0;
+            if($act_info['usertask'] == '主讲人' || $act_info['usertask'] == '项目负责人')
+            {
+                $weight = 3;
+            }
+            elseif($act_info['usertask'] == '助教' || $act_info['usertask'] == '召集人')
+            {
+                $weight = 1;
+            }
+            if($act_info['usertask'] == '主讲人')
+            {
+                $weight = 3;
+                $act_info['html_class'] = "ut1";
+            }
+            elseif($act_info['usertask'] == '助教')
+            {
+                $weight = 1;
+                $act_info['html_class'] = "ut2";
+            }
+            $member_act_total_count[$act_info['uid']]['uid'] = $act_info['uid'];
+            $member_act_total_count[$act_info['uid']]['user_act_count'] ++; 
+            $member_act_total_count[$act_info['uid']]['weight'] += $weight; 
+            $r1[$act_info['uid']] ++;
+            $r2[$act_info['uid']] += $weight;
+            $member_act[$act_info['uid']]['act'][$act_info['tid']]= $act_info;
+        }
+        $member_act_list_count = count($member_act);
+        array_multisort($r1, SORT_DESC, $r2, SORT_DESC, $member_act_total_count);
+        include template("ygclub/act_type_project_index");
+        exit;
+    }
+    else
+    {
+        $sql = 'SELECT count(p.tid) as p_count, p.class, t.typeid, c.name, c.displayorder  FROM ';
+        $sql .= DB::table('ygclub_party') . " as p ";
+        $sql .= "LEFT JOIN " . DB::table('forum_thread') . " as t on p.tid = t.tid ";
+        $sql .= "LEFT JOIN " . DB::table('forum_threadclass') . " as c on t.typeid = c.typeid where 1 ";
+        $sql .= "AND t.fid=2 ";
+        $sql .= "AND t.subject NOT LIKE '%活动取消%' ";
+        $sql .= "AND p.class = '阳光公益活动' ";
+        $sql .= "GROUP BY c.typeid ";
+        $sql .= "ORDER BY c.displayorder, p_count desc ";
+        $result = DB::fetch_all($sql);
+        
+        include template("ygclub/act_type_index");
+        exit;
+    }
+}
 
 // 展示所有人的活动统计
-$today = strtotime(date('Y-m-d')) + 86399;
 if ($starttime == '' || $endtime == '')
 {
     $starttime =date('Y-m-01',strtotime(date('Y',$today).'-'.(date('m',$today)-1).'-01'));
